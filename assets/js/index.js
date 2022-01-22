2f7ceba44ba3215a4d52944fb369a05a7179e35b
Tayyab Ferozi <ferozitayyab@gmail.com>
2022-01-22 05:18:25 +0500

    initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+document.addEventListener("scroll", function () {
+  let navbar = document.querySelector("#navbar");
+
+  let scrolledVal = document.querySelector("html").scrollTop;
+
+  if (scrolledVal > 0) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+
+AOS.init({ duration: 700, offset: 200 });
