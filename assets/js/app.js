c4575e6874ec819e361e2a0273aa6dc8e36dcf34
Tayyab Ferozi <ferozitayyab@gmail.com>
2022-02-14 09:13:16 +0500

    first page complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,7 +11,7 @@ window.addEventListener("load", function () {
     }
   });
 
-  document.querySelector("#preloader").remove();
+  $("#preloader").remove()
   AOS.init({ duration: 700, offset: 200 });
 
   const cursor = document.querySelector(".cursor");
@@ -45,34 +45,34 @@ window.addEventListener("load", function () {
 
   // CARDS ANIMATIONS
 
-  const container = document.querySelector("#why-us .cards .row > *");
-  const card = document.querySelector("#why-us .cards .card");
-  const title = document.querySelector("#why-us .cards .card h3");
-  // const santa = document.querySelector(".santa");
-  // const tree = document.querySelector(".tree");
+  // const container = document.querySelector("#why-us .cards .row > *");
+  // const card = document.querySelector("#why-us .cards .card");
+  // const title = document.querySelector("#why-us .cards .card h3");
+  // // const santa = document.querySelector(".santa");
+  // // const tree = document.querySelector(".tree");
 
-  //card rotate on mouse move
-  container.addEventListener("mousemove", (rotate) => {
-    let x = (innerWidth / 2 - rotate.pageX) / 50;
-    let y = (innerHeight / 2 - rotate.pageY) / 50;
+  // //card rotate on mouse move
+  // container.addEventListener("mousemove", (rotate) => {
+  //   let x = (innerWidth / 2 - rotate.pageX) / 50;
+  //   let y = (innerHeight / 2 - rotate.pageY) / 50;
 
-    card.style.transform = "rotateY(" + x + "deg) rotateX(" + y + "deg)";
-  });
+  //   card.style.transform = "rotateY(" + x + "deg) rotateX(" + y + "deg)";
+  // });
 
-  //card effects on mouse over the card
-  container.addEventListener("mouseover", () => {
-    title.style.transform = "translate3d(0, 0, 160px)";
-    // santa.style.transform = "translate3d(0, 0, 220px)";
-    // tree.style.transform = "translate3d(0, 0, 120px)";
-    card.style.transition = "all 0.1s ease";
-  });
+  // //card effects on mouse over the card
+  // container.addEventListener("mouseover", () => {
+  //   title.style.transform = "translate3d(0, 0, 160px)";
+  //   // santa.style.transform = "translate3d(0, 0, 220px)";
+  //   // tree.style.transform = "translate3d(0, 0, 120px)";
+  //   card.style.transition = "all 0.1s ease";
+  // });
 
-  //card back to normal when mouse out
-  container.addEventListener("mouseout", () => {
-    card.style.transform = "rotateY(0deg) rotateX(0deg)";
-    card.style.transition = "all 0.5s ease";
-    title.style.transform = "translate3d(0, 0, 0)";
-    // santa.style.transform = "translate3d(0, 0, 0)";
-    // tree.style.transform = "translate3d(0, 0, 0)";
-  });
+  // //card back to normal when mouse out
+  // container.addEventListener("mouseout", () => {
+  //   card.style.transform = "rotateY(0deg) rotateX(0deg)";
+  //   card.style.transition = "all 0.5s ease";
+  //   title.style.transform = "translate3d(0, 0, 0)";
+  //   // santa.style.transform = "translate3d(0, 0, 0)";
+  //   // tree.style.transform = "translate3d(0, 0, 0)";
+  // });
 });
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,29 @@
-var swiper = new Swiper(".testimonials-swiper", {
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    dynamicBullets: true,
-  },
+import { CountUp } from "./countUp.min.js";
+
+window.addEventListener("load", function () {
+  var swiper = new Swiper(".testimonials-swiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+  });
+
+  $("#faqs .question .q-statement").click(function () {
+    $this = $(this);
+    $this.parent().toggleClass("active");
+    $this.siblings(".answer").slideToggle();
+  });
+
+  let demo = new CountUp("count-up", 5245);
+  if (!demo.error) {
+    demo.start();
+    console.log("STARTED");
+  } else {
+    console.log("ERROR");
+    console.error(demo.error);
+  }
 });
