2769455220a21990eefcb8724efa5dacd2a7618e
Tayyab Ferozi <ferozitayyab@gmail.com>
2022-03-02 00:05:18 +0500

    frontend complete

---
+++ assets/js/sample.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+    prevEl: ".swiper-prev",
+    nextEl: ".swiper-next",
+  },
+  autoplay: true,
+  loop: true,
+  disableOnInteraction: false,
+});
---
+++ assets/js/success-stories.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  // loop: true,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2,
+    slideShadows: false,
+  },
+  navigation: {
+    nextEl: ".swiper-nav .next",
+    prevEl: ".swiper-nav .prev",
+  },
+});
