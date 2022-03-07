9148615bbc89e823f820dbb10f158861c9b16cda
Tayyab Ferozi <ferozitayyab@gmail.com>
2022-03-07 23:45:07 +0500

    adds logo in footer

--- assets/js/app.js
+++ assets/js/app.js
@@ -61,3 +61,21 @@ function toggleTawkTo() {
 }
 
 $(".btn").click(toggleTawkTo);
+
+var tlJsHost =
+  window.location.protocol == "https:"
+    ? "https://secure.trust-provider.com/"
+    : "http://www.trustlogo.com/";
+document.write(
+  unescape(
+    "%3Cscript src='" +
+      tlJsHost +
+      "trustlogo/javascript/trustlogo.js' type='text/javascript'%3E%3C/script%3E"
+  )
+);
+
+TrustLogo(
+  "https://www.positivessl.com/images/seals/positivessl_trust_seal_md_167x42.png",
+  "POSDV",
+  "none"
+);
